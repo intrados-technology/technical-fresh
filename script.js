f81fb228b4b437cf6c4f00323f25656d9cd386a9
intrados-technology <intrados.technology@gmail.com>
2026-09-12 17:50:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -152,7 +152,6 @@ const DOM = {
   rulesModal:         document.getElementById('rules-modal'),
   btnRulesUnderstood: document.getElementById('btn-rules-understood'),
 
-  refId:          document.getElementById('ref-id')
 };
 
 // ── Exam Rules Modal ──────────────────────────────────────────────
@@ -885,7 +884,6 @@ function finaliseSubmission() {
   DOM.assSection.style.display   = 'none';
   DOM.timerDisplay.style.display = 'none';
   DOM.confSection.style.display  = 'block';
-  DOM.refId.textContent = scores.referenceId;
 }
 
 // ── Google Sheets ─────────────────────────────────────────────────
